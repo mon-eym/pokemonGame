b4ff684bc564c629963fc467aebadbc259038c08
John Eymard <John Eymard@LAPTOP-C79STBON>
2025-11-23 19:32:04 +0800

    Initial commit of pokemon game

--- pokemonGame/Program.cs
+++ pokemonGame/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+       static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Pokémon Game!");
+        Console.WriteLine("Choose your starter Pokémon:");
+        Console.WriteLine("1. Bulbasaur");
+        Console.WriteLine("2. Charmander");
+        Console.WriteLine("3. Squirtle");
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                Console.WriteLine("You chose Bulbasaur!");
+                break;
+            case "2":
+                Console.WriteLine("You chose Charmander!");
+                break;
+            case "3":
+                Console.WriteLine("You chose Squirtle!");
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please restart the game.");
+                break;
+        }
+        // Additional game logic would go here
+    }
+}
